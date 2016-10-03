c84ca9823becb2acbb6d5ff644acb6b6b10ef81d
nic3r <m.karolyi@tricentis.com>
2016-10-03 12:49:27 +0200

    first_checkin

---
+++ PROG1/src/bwi/prog1B/WS1617/wi16b076_Karolyi/ExerciseSheet00/HelloWorld.java
@@ -0,0 +1,10 @@
+package bwi.prog1B.WS1617.wi16b076_Karolyi.ExerciseSheet00;
+import bwi.prog.utils.TextIO;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TextIO.put("Hello world!\n");
+	}
+
+}
---
+++ PROG1/src/bwi/prog1B/WS1617/wi16b076_Karolyi/ExerciseSheet00/NumberFormatChecker.java
@@ -0,0 +1,10 @@
+package bwi.prog1B.WS1617.wi16b076_Karolyi.ExerciseSheet00;
+import bwi.prog.utils.TextIO;
+public class NumberFormatChecker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TextIO.putf("%f\n", 3.14159);
+	}
+
+}
